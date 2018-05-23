03dab5d05fac331246546f9bfafe5567c601c4df
ingochris <ingochris@gmail.com>
2018-05-22 23:02:42 -0700

    RM: popup-gallery.js

--- js/main.js
+++ js/main.js
@@ -57,8 +57,7 @@
   }, 300);
 
   // Magnific popup calls
-  $('.popup-gallery').magnificPopup({
-    delegate: 'a',
+/*    delegate: 'a',
     type: 'image',
     tLoading: 'Loading image #%curr%...',
     mainClass: 'mfp-img-mobile',
@@ -71,5 +70,6 @@
       tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
     }
   });
+*/
 
 })(jQuery); // End of use strict
